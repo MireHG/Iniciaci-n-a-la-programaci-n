57fb6de44b868445def55ec3cb59bb305338f944
Mireya Harillo <mireyaharillo@MacBook-Air-de-Mireya.local>
2022-11-17 21:21:01 +0100

    Variables

---
+++ htdocs/00-variables/index.php
@@ -0,0 +1,40 @@
+<?php
+// Usamos echo para mostrar por pantalla.
+// Ejemplo: echo "Hello World!";
+
+// VARIABLES: es un espacio para almacenar información.
+
+
+$string = "Hello world!";
+
+$int = 1;
+
+$float = 0.5;
+
+$text = "Hello" . "world";
+
+echo $string;
+
+
+//TIPOS DE DATOS
+
+//String: texto
+
+$userName = "Dani";
+
+//int = Números enteros
+
+$age = 18;
+
+// float = Números decimales
+
+$height = 1.10;
+
+//Boolean = True / False
+
+$isaHuman = True;
+
+// Array = Colección de datos
+
+$fruits = ["banana", "apple", 1];
+
